35aad9f79c8b36313b4a98f6a0265e3aebff3793
Riven <anshkreaction01@gmail.com>
2026-06-27 07:45:42 +0530

    Update lexer.hpp

--- compiler/Include/lexer/lexer.hpp
+++ compiler/Include/lexer/lexer.hpp
@@ -3,3 +3,23 @@
 #include "../Token.h"
 
 class Lexer
+{
+public: 
+Lexer(const std::string& source) 
+std::vector<Token>Tokenize();
+
+private:
+
+std::string Source;
+size_t Position;
+
+char Peek() const;
+char Advance();
+
+void SkipWhiteSpace();
+
+Token ReadIndentifier();
+Token Readstring();
+
+};
+
