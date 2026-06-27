c0a953a49da8d6bdb42c58131f0b966ddb9cce18
Riven <anshkreaction01@gmail.com>
2026-06-27 11:35:40 +0530

    Create Token.hpp

---
+++ compiler/include/common/Token.hpp
@@ -0,0 +1,37 @@
+#pragma once 
+#include <string>
+
+enum class TokenType
+{
+
+FILE, 
+MAIN, 
+
+LEFT_BRACKET,
+RIGHT_BRACKET, 
+
+LEFT_BRACE,
+RIGHT_BRACE, 
+
+IMPR, 
+OUTPUT, 
+
+STRING, 
+SEMICOLON, 
+EOF, 
+
+
+};
+
+
+struct Token 
+{
+TokenType token;
+std::string text;
+
+Token(TokenType token, const std::string& text)
+: text(text) 
+  type(type) 
+
+
+};
